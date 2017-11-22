4713fd2a4c1e18db1d90c25aaaa979ea5208cf45
Myron Navarrete <myronnavarrete@gmail.com>
2017-11-21 21:44:20 -0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+var verbing, holiday, person1, place, adj1, adj2, animals, noun1, person2, verb1, verbed, adj3, verb2, nouns, pastVerb, noun2, noun3, person3, person4;
+
+var madLibs = `Ok so I was <span class='red'>${verbing}</span>! I'm an adult, 
+I'm supposed to snoop, especially around 
+<span class='red'>${holiday}</span> time! What <span class='red'>${person1}</span> doesn't 
+snoop? I just so happened to be in our 
+<span class='red'>${place}</span>. Normally I'm not up there, it's 
+<span class='red'>${adj1}</span> and <span class='red'>${adj2}</span>. Plus there are 
+<span class='red'>${animals}</span>, yuck! I couldnt help myself! 
+There it was right by my little sister's 
+old <span class='red'>${noun1}</span>. Guess <span class='red'>${person2}</span> didn't think 
+I would <span class='red'>${verb1}</span> it. I <span class='red'>${verbed}</span> it, 
+it felt <span class='red'>${adj3}</span>. Should I <span class='red'>${verb2}</span> it? 
+Looking closer I saw some <span class='red'>${nouns}</span>. 
+I got even closer and <span class='red'>${pastVerb}</span> a 
+<span class='red'>${noun2}</span>! The hidden <span class='red'>${noun2}</span> was alive! 
+A yelp came from the <span class='red'>${noun2}</span>. I opened the <span class='red'>${noun2}</span> 
+and to my surprise a <span class='red'>${noun3}</span> was inside! 
+Guess who else was hidden in the <span class='red'>${place}</span>? 
+<span class='red'>${person3}</span> and <span class='red'>${person4}</span> just knew I was 
+<span class='red'>${verbing}</span>!`;
+
+
+
+
+
+
+
+
+
